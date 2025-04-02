82415120a8b81a92bebe58924ba9f93dcb008e35
Rajesh80085 <rajesh0143225@gmail.com>
2025-04-02 20:34:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/e29aa852-4be2-4dca-8ef4-ba9be6744820.jpeg')).split()
 
 
 # Admins & Users
